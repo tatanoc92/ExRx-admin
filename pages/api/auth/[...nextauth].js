49bdedf726869561503b570b9c189aa7a472fa0c
tatanoc92 <whardy@iorga.com>
2023-08-29 17:50:38 +0200

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['dawid.paszko@gmail.com'];
+const adminEmails = ['william@nginefitness.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
